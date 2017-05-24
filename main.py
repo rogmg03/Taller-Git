5f24aae301d36b6f9f15a46ed9f3f667b9d3631d
Roger Mora <rogermora03@gmail.com>
2017-05-24 10:11:07 -0600

    [new] About Button

--- main.py
+++ main.py
@@ -13,5 +13,11 @@ root.maxsize(500,500)
 #etiquetatexto = Label(WidgetContenedor, text=Contenido, font = (Tipo,Tamanno), bg=ColorDeFondo, fg=ColorDeLetra, width, height)
 welcome_lbl = Label(root, text = "Bienvenidos al taller de GIT", font = ("calibri","18"), fg = "#000b98", width= 28, height = 1)
 welcome_lbl.place(x = 20, y = 20)
+
+def fetchAboutView():
+    display_about_view()
+
+about_btn = Button(root, text="Sobre este proyecto", command=fetchAboutView, bg = "#000000", fg = "#ffffff", width = 50, height = 5)
+about_btn.place(x=35,y=180)
  
 root.mainloop()
